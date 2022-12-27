8f8cfcfbf68138ce56eb135db38991eddee20128
Daniel Mateu <danielmateu86@gmail.com>
2022-12-27 16:22:46 +0100

    Instalación prisma

--- README.md
+++ README.md
@@ -40,3 +40,7 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/deploym
 - Tailwindcss
 - Postcss
 - Autoprefixer
+
+#### ORM
+
+- Prisma
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 import { Inter } from '@next/font/google'
 // import styles from '../styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
