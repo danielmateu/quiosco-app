60ab6a8159bdaa594c7cae1c068adf1e04364a4d
Daniel Mateu <danielmateu86@gmail.com>
2022-12-28 14:23:54 +0100

    Detectando la categoria actual

--- components/Categoria.jsx
+++ components/Categoria.jsx
@@ -1,10 +1,14 @@
 import Image from "next/image"
+import useQuiosco from "../hooks/useQuiosco"
 
 export const Categoria = ({categoria}) => {
     
-    const {nombre, icono, id} = categoria
+    const {categoriaActual, handleClickCategoria} = useQuiosco();
+
+    const {nombre, icono, id} = categoria;
+
     return (
-        <div className="flex gap-2 items-center m-2  p-5 hover:bg-orange-200 transition-all">
+        <div className={`${categoriaActual?.id === id ? 'bg-orange-200' : ''} flex gap-4 items-center m-2 p-4 w-fit hover:bg-orange-200 rounded-xl transition-all`}>
             <Image
                 width={50}
                 height={50}
@@ -16,6 +20,7 @@ export const Categoria = ({categoria}) => {
             <button
                 type="button"
                 className="text-2xl hover:cursor-pointer hover:text-slate-500 transition-all"
+                onClick={() => handleClickCategoria(id)}
             >
                 {nombre}
             </button>
--- context/QuiscoProvider.jsx
+++ context/QuiscoProvider.jsx
@@ -6,7 +6,8 @@ const QuioscoContext = createContext();
 
 const QuioscoProvider = ({ children }) => {
 
-    const [categorias, setCategorias] = useState([])
+    const [categorias, setCategorias] = useState([]);
+    const [categoriaActual, setCategoriaActual] = useState({})
 
     const obtenerCategorias = async() => {
         const {data} = await axios('/api/categorias')
@@ -15,14 +16,20 @@ const QuioscoProvider = ({ children }) => {
 
     useEffect(() => {
         obtenerCategorias()
-    }, [])
-    
+    }, []);
+
+    const handleClickCategoria = id => {
+        const categoria = categorias.filter(cat => cat.id === id);
+        setCategoriaActual(categoria[0]);
+    }
 
     return (
 
         <QuioscoContext.Provider
             value={{
                 categorias,
+                categoriaActual,
+                handleClickCategoria,
             }}
         >
             {children}
