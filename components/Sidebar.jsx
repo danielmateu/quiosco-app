4bcab846a613e58957b666ca1be95018cbd58aa1
Daniel Mateu <danielmateu86@gmail.com>
2022-12-28 11:42:44 +0100

    Agregando imagenes

---
+++ components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image"
+
+
+
+export const Sidebar = () => {
+
+
+    return (
+        <>
+            <Image width={300} height={100} src='/assets/img/logo.svg' alt='imagen logo' />
+
+            
+        </>
+    )
+}
